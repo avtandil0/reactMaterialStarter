827a5615fc9effa3d5e8944ebfcd57bbfe151b43
avtandil <avtandilzenaishvili@gmail.com>
2020-07-01 22:32:16 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MiniDrawer from './components/drawer.js'
+
+function App() {
+  return (
+    <div className="App">
+      <MiniDrawer/>
+    </div>
+  );
+}
+
+export default App;
